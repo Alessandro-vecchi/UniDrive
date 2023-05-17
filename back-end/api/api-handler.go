7c393b05ee239e9d6558d67225079e66d5a3faf9
Alessandro Vecchi <vecchi.1938069@studenti.uniroma1.it>
2023-05-17 10:17:27 +0200

    adding routes

--- back-end/api/api-handler.go
+++ back-end/api/api-handler.go
@@ -7,7 +7,11 @@ import (
 // RegisterRoutes registers the API routes
 func (h *Handler) RegisterRoutes(r *gin.Engine) {
 	r.GET("/ping", h.ping)
-	r.POST("/feedback", h.createReview)
+
+	r.POST("/feedback/", h.createReview)
 	r.GET("/feedback/:id", h.getReview)
+
+	r.POST("/profile/", h.crateANewUser)
+	r.GET("/profile/:id", h.getUserProfile)
 
 }

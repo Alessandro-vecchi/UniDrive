59a7573a9c0752ffe1af250d89f9dd9f53e68885
Alessandro Vecchi <vecchi.1938069@studenti.uniroma1.it>
2023-05-21 23:30:19 +0200

    fixed

--- back-end/api/get-rides.go
+++ back-end/api/get-rides.go
@@ -2,8 +2,8 @@ package api
 
 import (
 	"UniDrive/back-end/database"
+	"fmt"
 	"net/http"
-	"strconv"
 	"strings"
 
 	"github.com/gin-gonic/gin"
@@ -15,32 +15,30 @@ func (h *Handler) getRides(c *gin.Context) {
 	from := c.Query("origin")
 	to := c.Query("destination")
 	date := c.Query("date")
-	at_hour := c.Query("hour")
-	at_h, _ := strconv.Atoi(at_hour)
-	at_m := c.Query("mins")
-	at_min, _ := strconv.Atoi(at_m)
+	at_h := c.Query("hour")
+	at_min := c.Query("min")
 
 	// Retrieve the DB instance from the context
 	db, exists := c.Get("DB")
 	if !exists {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Database connection not found"})
+		c.JSON(http.StatusInternalServerError, gin.H{"message": "Database connection not found"})
 		return
 	}
 
 	// Access the DB instance using type assertion
 	gormDB, ok := db.(*gorm.DB)
 	if !ok {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Invalid database connection type"})
+		c.JSON(http.StatusInternalServerError, gin.H{"message": "Invalid database connection type"})
 		return
 	}
 	// Convert the search parameters to lowercase
 	from = strings.ToLower(from)
 	to = strings.ToLower(to)
-	date_time := date + " " + strconv.Itoa(at_h) + ":" + strconv.Itoa(at_min) // 2006-01-02 15:30
-
+	date_time := date + " " + at_h + ":" + at_min // 2006-01-02 15:30
+	fmt.Println(date_time, from, to)
 	rides, err := database.SearchRides(gormDB, from, to, date_time)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to search for rides"})
+		c.JSON(http.StatusInternalServerError, gin.H{"message": "Failed to search for rides", "error": err.Error()})
 		return
 	}
 
--- back-end/database/get-ride-by-id.go
+++ back-end/database/get-ride-by-id.go
@@ -2,20 +2,17 @@ package database
 
 import (
 	"UniDrive/back-end/api/models"
-	"fmt"
 
 	"github.com/jinzhu/gorm"
-	
 )
 
 func GetRideByID(db *gorm.DB, id string) (models.Ride, error) {
 	var ride models.Ride
 	result := db.Raw("SELECT * FROM ride WHERE id = ?", id).Scan(&ride)
-	fmt.Println(ride)
+
 	if result.Error != nil {
 		return models.Ride{}, result.Error
 	}
 
-	
 	return ride, nil
 }

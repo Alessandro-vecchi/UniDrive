85d25fb1555df22c294ebb3dd3cb4968eebcaaee
Alessandro Vecchi <vecchi.1938069@studenti.uniroma1.it>
2023-06-01 12:32:19 +0200

    fixing rides

--- back-end/api/models/ride.go
+++ back-end/api/models/ride.go
@@ -1,11 +1,20 @@
 package models
 
 type Ride struct {
-	ID             string `gorm:"primary_key" json:"id"`
-	Origin         string `json:"origin"`
-	Destination    string `json:"destination"`
-	DepartDatetime string `json:"depart_datetime"`
-	DriverName     string `json:"driver_name"`
-	DriverID       string `gorm:"not null;foreignkey:ProfileDBID" json:"driver_id"`
-	AvailableSeats int    `json:"available_seats"`
+	ID             string       `gorm:"primary_key" json:"id"`
+	Origin         string       `json:"origin"`
+	Destination    string       `json:"destination"`
+	DepartDatetime string       `json:"depart_datetime"`
+	DriverProfile  ShortProfile `json:"driver_profile"`
+	MeetingPoint   string       `json:"meeting_point"`
+	MeetingTime	string       `json:"meeting_time"`
+}
+
+type ShortProfile struct {
+	UserID            string  `gorm:"primary_key" json:"user_id"`
+	ProfilePictureUrl string  `json:"profile_picture_url"`
+	Name              string  `json:"name"`
+	Surname           string  `json:"surname"`
+	Rating            float64 `json:"rating"`
+	AvailableSeats    int     `json:"available_seats"`
 }

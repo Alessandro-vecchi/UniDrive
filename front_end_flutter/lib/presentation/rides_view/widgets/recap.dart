e1364789ecec2e2786be1f8b7fffdabba9eb2464
Alessandro Vecchi <vecchi.1938069@studenti.uniroma1.it>
2023-06-07 00:12:58 +0200

    quick changes

--- front_end_flutter/lib/presentation/rides_view/widgets/recap.dart
+++ front_end_flutter/lib/presentation/rides_view/widgets/recap.dart
@@ -5,130 +5,123 @@ import '../../form_fields/date_picker_field.dart';
 import '../../search_box.dart';
 
 class Recap extends StatelessWidget {
-
   const Recap({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Container(
-          padding: const EdgeInsets.all(16),
-          decoration: BoxDecoration(
-            color: const Color(0xFF222227),
-            borderRadius: BorderRadius.circular(8),
+    return Container(
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: const Color(0xFF222227),
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBox(height: 8),
+          _buildRow(
+            context,
+            "From",
+            const Expanded(
+              flex: 5,
+              child: SearchBox(formControlName: 'origin'),
+            ),
           ),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.start,
+          const SizedBox(height: 8),
+          _buildRow(
+            context,
+            "to",
+            const Expanded(
+              flex: 5,
+              child: SearchBox(formControlName: 'destination'),
+            ),
+          ),
+          const SizedBox(height: 8),
+          Row(
             children: [
-              const SizedBox(height: 8),
-              Row(
-                children: [
-                  Expanded(
-                    child: Align(
-                      alignment: Alignment.center,
-                      child: Text(
-                        'From',
-                        style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                            ),
-                      ),
-                    ),
+              Expanded(
+                child: Align(
+                  alignment: Alignment.center,
+                  child: Text(
+                    'Arrive By',
+                    style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
                   ),
-                  const Expanded(
-                    flex: 5,
-                    child: SearchBox(formControlName: 'origin'),
-                  ),
-                ],
+                ),
               ),
-              const SizedBox(height: 8),
-              Row(
-                children: [
-                  Expanded(
-                    child: Align(
-                      alignment: Alignment.center,
-                      child: Text(
-                        'To',
-                        style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                              fontWeight: FontWeight.bold,
-                            ),
-                      ),
-                    ),
-                  ),
-                  const Expanded(
-                    flex: 5,
-                    child: SearchBox(formControlName: 'destination'),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 8),
-              Row(
-                children: [
-                  Expanded(
-                    child: Align(
-                      alignment: Alignment.center,
-                      child: Text(
-                        'Arrive By',
-                        style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                            ),
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    child: TimePickerField(
-                      formControlName: 'time',
-                      style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                            color: Colors.black,
-                          ),
-                      decoration: const InputDecoration(
-                        hintText: 'Enter time',
-                        hintStyle: TextStyle(color: Colors.grey, fontSize: 12),
-                        filled: true,
-                        fillColor: Colors.white,
-                      ), label: 'Time',
-                    ),
-                  ),
-                  Expanded(
-                    child: Align(
-                      alignment: Alignment.center,
-                      child: Text(
-                        'On',
-                        style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                            ),
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    flex: 2,
-                    child: DatePickerField(
-                      formControlName: 'date',
-                      style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+              Expanded(
+                child: TimePickerField(
+                  formControlName: 'time',
+                  style: Theme.of(context).textTheme.bodyMedium?.copyWith(
                         color: Colors.black,
                       ),
-                      decoration: const InputDecoration(
-                        hintText: 'Enter date',
-                        hintStyle: TextStyle(color: Colors.grey, fontSize: 12),
-                        filled: true,
-                        fillColor: Colors.white,
-                      ),
-                      firstDate: DateTime.now(),
-                      lastDate: DateTime(9999, 12, 31),
-                      label: 'Date',
-                      languageCode: 'en',
-                    ),
+                  decoration: const InputDecoration(
+                    hintText: 'Enter time',
+                    hintStyle: TextStyle(color: Colors.grey, fontSize: 12),
+                    filled: true,
+                    fillColor: Colors.white,
                   ),
-                ],
+                  label: 'Time',
+                ),
+              ),
+              Expanded(
+                child: Align(
+                  alignment: Alignment.center,
+                  child: Text(
+                    'On',
+                    style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: DatePickerField(
+                  formControlName: 'date',
+                  style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                        color: Colors.black,
+                      ),
+                  decoration: const InputDecoration(
+                    hintText: 'Enter date',
+                    hintStyle: TextStyle(color: Colors.grey, fontSize: 12),
+                    filled: true,
+                    fillColor: Colors.white,
+                  ),
+                  firstDate: DateTime.now(),
+                  lastDate: DateTime(9999, 12, 31),
+                  label: 'Date',
+                  languageCode: 'en',
+                ),
               ),
             ],
           ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildRow(BuildContext context, String title, Widget child) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Expanded(
+          child: Align(
+            alignment: Alignment.center,
+            child: Text(
+              title,
+              style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                    color: title == 'From' ? Colors.white : null,
+                    fontWeight: FontWeight.bold,
+                  ),
+            ),
+          ),
         ),
+        child,
       ],
     );
   }
